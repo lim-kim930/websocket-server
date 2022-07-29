672e1726a7e2f6e1fe43a6cb73710e11158e9167
lim-kim930 <1625753207lim@gmail.com>
2022-07-29 19:30:42 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,38 @@
 import WebSocket, { WebSocketServer } from 'ws';
 
 const wss = new WebSocketServer({
-  port: 3001,
-//   perMessageDeflate: {
-//     zlibDeflateOptions: {
-//       // See zlib defaults.
-//       chunkSize: 1024,
-//       memLevel: 7,
-//       level: 3
-//     },
-//     zlibInflateOptions: {
-//       chunkSize: 10 * 1024
-//     },
-//     // Other options settable:
-//     clientNoContextTakeover: true, // Defaults to negotiated value.
-//     serverNoContextTakeover: true, // Defaults to negotiated value.
-//     serverMaxWindowBits: 10, // Defaults to negotiated value.
-//     // Below options specified as default values.
-//     concurrencyLimit: 10, // Limits zlib concurrency for perf.
-//     threshold: 1024 // Size (in bytes) below which messages
-//     // should not be compressed if context takeover is disabled.
-//   }
+    port: 3001,
+    //   perMessageDeflate: {
+    //     zlibDeflateOptions: {
+    //       // See zlib defaults.
+    //       chunkSize: 1024,
+    //       memLevel: 7,
+    //       level: 3
+    //     },
+    //     zlibInflateOptions: {
+    //       chunkSize: 10 * 1024
+    //     },
+    //     // Other options settable:
+    //     clientNoContextTakeover: true, // Defaults to negotiated value.
+    //     serverNoContextTakeover: true, // Defaults to negotiated value.
+    //     serverMaxWindowBits: 10, // Defaults to negotiated value.
+    //     // Below options specified as default values.
+    //     concurrencyLimit: 10, // Limits zlib concurrency for perf.
+    //     threshold: 1024 // Size (in bytes) below which messages
+    //     // should not be compressed if context takeover is disabled.
+    //   }
 });
 
-wss.on("listening", ()=>{
+wss.on("listening", () => {
     console.log("serve is running on 3001");
 });
 
-wss.on('connection',(client, req)=>{
+wss.on('connection', (client, req) => {
     console.log(req.headers.origin! + " 连接成功");
-	client.on("message", (data)=>{
-        console.log("msg", data.toString()); 
+    client.on("message", (data) => {
+        console.log("msg", data.toString());
         // console.log(JSON.parse(data.toString()));
-        setInterval(()=>{
+        setInterval(() => {
             client.send("6666666");
         }, 1000)
     });
